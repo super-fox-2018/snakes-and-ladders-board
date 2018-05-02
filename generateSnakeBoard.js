0f59ef22491806b3eb37ac88ce2e2c19cd20ebb1
David Yunus <dove.fit@gmail.comgit config --global user.name>
2018-05-02 16:55:36 +0700

    generate snake ladder board

---
+++ generateSnakeBoard.js
@@ -0,0 +1,34 @@
+'use strict'
+
+function generateBoard(num) {
+    var counter = 1;
+    var board = [];
+
+    for(let i = 0; i < num; i++) {
+        board.push([]);
+    }
+
+    var status = true;
+
+    for(let i = board.length - 1; i >= 0; i--) {
+        for(let j = 1; j <= num; j++) {
+            if(status == true) {
+                board[i].push(counter);
+                counter++;
+                if(j % num == 0) {
+                    status = false;
+                }
+            } else if(status == false) {
+                board[i].unshift(counter);
+                counter++;
+                if(j % num == 0) {
+                    status = true;
+                }
+            }
+        }
+    }
+
+    return board;
+}
+
+console.log(generateBoard(8));
