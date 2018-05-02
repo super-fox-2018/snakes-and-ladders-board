271551a4766220833c8635577c57aeafd88e281f
Rhesa Utomo <venderblitz@gmail.com>
2018-05-02 16:27:41 +0700

    add condition if paramater is and odd number

--- index.js
+++ index.js
@@ -32,10 +32,33 @@ function generateBoard(num) {
       output.push(steps);
     }
   }
+  else {
+    for (let i = 0; i < num; i++) {
+      var steps = [];
+
+      if (i % 2 === 0) {
+        lastNum = lastNum - num + 1;
+        for (let j = 0; j < num; j++) {
+          steps.push(lastNum);
+          lastNum++
+        }
+      }
+      else {
+        lastNum = lastNum - num - 1;
+        for (let j = 0; j < num; j++) {
+          steps.push(lastNum);
+          lastNum--;
+        }
+      }
+
+    output.push(steps);
+    }
+  }
 
   return output;
 }
 
 
 console.log(generateBoard(10));
+console.log(generateBoard(8));
 console.log(generateBoard(15));
