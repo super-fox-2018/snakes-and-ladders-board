a2e268d2da944be7dba66902d812e6f49dbd513e
ridozaen <ridozaen@gmail.com>
2018-05-02 18:30:32 +0700

    modify snakes and ladders

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
 function generateBoard(num) {
-    var arrNew = generateArray(num, num);
-    let angka = num * num;
-    for (let i = 0; i < arrNew.length; i++) {
-        if (num%2 === 0) {
-            if (i % 2 === 0) {
-                for (let j = 0; j < arrNew[i].length; j++) {
-                    arrNew[i][j] = angka;
-                    angka--;
-                }
-            } else {
-                for (let k = arrNew[i].length - 1; k >= 0; k--) {
-                    arrNew[i][k] = angka;
-                    angka--;
-                }
+    var arrNew = generateArray(num,num);
+    let angka = num*num;
+    for (let i = 0; i < arrNew.length; i++){
+        if (i%2 === 0 && num%2 === 0){
+            for (let j = 0; j < arrNew[i].length; j++){
+                arrNew[i][j] = angka;
+                angka--;
             }
-        }
-
-        if (num%2 !== 0) {
-            if (i % 2 !== 0) {
-                for (let j = 0; j < arrNew[i].length; j++) {
-                    arrNew[i][j] = angka;
-                    angka--;
-                }
-            } else {
-                for (let k = arrNew[i].length - 1; k >= 0; k--) {
-                    arrNew[i][k] = angka;
-                    angka--;
-                }
+        }else{
+            for (let k = arrNew[i].length - 1; k >= 0; k--){
+                arrNew[i][k] = angka;
+                angka--;
             }
         }
     }
-    return arrNew;
+    return arrNew; 
 }
 
-function generateArray(row, col) {
+function generateArray(row, col){
     let arrColumn = [];
-    for (let i = 0; i < row; i++) {
+    for (let i=0; i < row; i++){
         let arrRow = [];
-        for (let j = 0; j < col; j++) {
+        for (let j=0; j < col; j++){
             arrRow.push(0);
         }
         arrColumn.push(arrRow);
